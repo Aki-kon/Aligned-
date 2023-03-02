4256bab556573317060dd0bbcb751d169fb3ce3e
Aki-kon <118151517+Aki-kon@users.noreply.github.com>
2023-03-01 19:10:12 -0800

    agregamos app

---
+++ TEMPLATE/Cabecera.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FUTURE NOW</title>
+    <link rel="stylesheet" href="./CSS2.0/bootstrap.min.css" />
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-black bg-light">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">FUTURE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">INICIO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">LIBROS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.php">NOSOTROS</a>
+            </li>
+        </ul>
+    </nav>
+<div class="conteiner">
+    <div class="row"> 
