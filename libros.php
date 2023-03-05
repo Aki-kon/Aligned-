961d14cf2616d1da331bdc4209277f4abaf4a546
Aki-kon <118151517+Aki-kon@users.noreply.github.com>
2023-03-04 21:39:21 -0800

    cambios a la cabecera

--- libros.php
+++ libros.php
@@ -1,35 +1,3 @@
 <?php include("template/cabecera.php");?>
-<div class="col-md-3">
-<div class="card">
-    <img class="card-img-top" src="https://thumbs.dreamstime.com/b/estilo-plano-del-icono-libro-de-misterios-antiguo-ilustraci%C3%B3n-plana-viejo-vector-misterio-para-el-dise%C3%B1o-web-198556395.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Misterio</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-    </div>
-</div>  
-</div>
-<div class="card">
-    <img class="card-img-top" src="https://thumbs.dreamstime.com/b/estilo-plano-del-icono-libro-de-misterios-antiguo-ilustraci%C3%B3n-plana-viejo-vector-misterio-para-el-dise%C3%B1o-web-198556395.jpg" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Misterio</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-    </div>
-</div>  
-</div>
-<div class="card">
-    <img class="card-img-top" src="" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Misterio</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-    </div>
-</div>  
-</div>
-<div class="card">
-    <img class="card-img-top" src="" alt="">
-    <div class="card-body">
-        <h4 class="card-title">Misterio</h4>
-        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-    </div>
-</div>  
-</div>
+
 <?php include("template/pie.php");?>
